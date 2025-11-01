a1c6844b6ed2309b33cbbd9d85b895592f81949c
KaushalyaAberathna <147972607+KaushalyaAberathna@users.noreply.github.com>
2025-11-01 22:34:42 +0530

    connected to the database

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
-import express from 'express'
+import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import Student from './models/student.js';
+
 
 let app = express()
+mongoose.connect("mongodb+srv://admin:321@cluster0.otwbffy.mongodb.net/?appName=Cluster0").then(
+    ()=>{
+       console.log ("connected to the database");
+    }
+).catch(
+    ()=>{
+        console.log("Connection Failed");
+    }
+)
+//mongodb+srv://admin:321@cluster0.otwbffy.mongodb.net/?appName=Cluster0
 
 app.use(bodyParser.json());
 
 app.get("/",
     (req,res)=>{
-        console.log(req.body);
-        console.log("Get request received");
-        res.json({
-            message:"Hello World"
-        })
+      Student.find().then(
+        (students)=>{
+            res.json(students)
+        }
+      ).catch(
+        ()=>{
+            res.json({
+                message:"An error occured"
+            })
+            
+        }
+      )
+    }
+)
+
+app.post("/",
+        (req,res)=>{
+         const student = new Student(req.body);
+
+         student.save().then(
+            ()=>{
+                res.json({
+                    message:"student saved successfully"
+                })
+            }
+        ).catch(
+            ()=>{
+                res.json({
+                    mesage:"Student save failed"
+                })
+            }
+        )
+
     }
 )
 
